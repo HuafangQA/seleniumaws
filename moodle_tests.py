a7e27ce960942797c4847839810bc3b01b674a78
zhf <hfloodnew0522@gmail.com>
2022-02-16 09:24:11 -0800

    uncomment some test

--- moodle_tests.py
+++ moodle_tests.py
@@ -12,20 +12,20 @@ class MoodleAppPositiveTestCases(unittest.TestCase):
         methods.log_out()
         # methods.tearDown()
 
-    # @staticmethod
-    # def test_b_create_new_user():
-    #     methods.setUp()
-    #     methods.log_in()
-    #     methods.create_new_user()
-    #     methods.logger()
-    #     methods.check_user_created()
-    #     methods.log_out()
-    #     methods.log_in(locators.username, locators.password)
-    #     methods.check_logged_in_with_new_cred()
-    #     methods.log_out()
-    #     methods.log_in()
-    #     methods.delete_new_user()
-    #     methods.log_out()
-    #     methods.tearDown()
+    @staticmethod
+    def test_b_create_new_user():
+        methods.setUp()
+        methods.log_in()
+        methods.create_new_user()
+        methods.logger()
+        methods.check_user_created()
+        methods.log_out()
+        methods.log_in(locators.username, locators.password)
+        methods.check_logged_in_with_new_cred()
+        methods.log_out()
+        methods.log_in()
+        methods.delete_new_user()
+        methods.log_out()
+        methods.tearDown()
 
 
